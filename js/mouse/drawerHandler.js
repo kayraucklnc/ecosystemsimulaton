9e94d6072b9bed42eeb675e5adf6a5b7f70907fe
İrem Ünsal <irem_unsal@windowslive.com>
2021-12-14 21:19:59 +0300

    Template Save/Load

--- js/mouse/drawerHandler.js
+++ js/mouse/drawerHandler.js
@@ -41,6 +41,7 @@ class DrawerHandler {
         }
 
     }
+
 }
 
 let drawHandler = new DrawerHandler();
--- js/util/domElementHandlers.js
+++ js/util/domElementHandlers.js
@@ -1,32 +1,101 @@
-function simulationToggle(){
+import * as Objects from "../world/Objects.js";
+import * as THREE from "../library/three.js-r135/build/three.module.js";
+import * as Materials from "../world/Materials.js";
+
+document.getElementById("saveButton").addEventListener("click", () => {
+    saveTemplate()
+});
+document.getElementById("loadButton").addEventListener("click", () => {
+    loadTemplate()
+});
+document.getElementById("brush").addEventListener("change", () => {
+    brushChange()
+});
+document.getElementById("eraser").addEventListener("change", () => {
+    eraserChange()
+});
+
+function simulationToggle() {
     document.getElementById("stop-sim-button").innerText = isSimActive ? "Start Simulation" : "Pause Simulation";
     isSimActive = !isSimActive;
 }
 
-function simulationSpeedChange(){
+function simulationSpeedChange() {
     simulation.timeScale = document.getElementById("timescale-slider").value;
 }
 
-function brushChange(){
-    if(document.getElementById("brush").checked){
+function brushChange() {
+    if (document.getElementById("brush").checked) {
         document.getElementById("eraser").checked = false;
         drawMode.brush = true;
         drawMode.eraser = false;
         orbitControls.enabled = false;
-    }else {
+    } else {
         drawMode.brush = false;
         orbitControls.enabled = true;
     }
 
 }
-function eraserChange(){
-    if(document.getElementById("eraser").checked){
+
+function eraserChange() {
+    if (document.getElementById("eraser").checked) {
         document.getElementById("brush").checked = false;
         drawMode.eraser = true;
         drawMode.brush = false;
         orbitControls.enabled = false;
-    }else {
+    } else {
         drawMode.eraser = false;
         orbitControls.enabled = true;
     }
+}
+
+function saveTemplate() {
+    let toSave = {arr: [], scale: parameters.plane.scale,}
+
+
+    for (let i = 0; i < world.grid.matrix.length; i++) {
+        for (let y = 0; y < world.grid.matrix[i].length; y++) {
+            if (world.grid.matrix[i][y] == null) {
+                toSave.arr.push(null);
+            } else {
+                toSave.arr.push(world.grid.matrix[i][y].constructor.name);
+            }
+        }
+    }
+    console.log(JSON.stringify(toSave));
+    //  download("template.json",JSON.stringify({ matrix: world.grid.matrix }));
+
+    //console.log(world.grid.matrix[1]);
+
+    //
+}
+
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
+async function loadTemplate() {
+    let [fileHandle] = await window.showOpenFilePicker();
+    let fileData = await fileHandle.getFile();
+    let text = await fileData.text();
+    let parsedData = JSON.parse(text);
+
+    let length = parsedData["matrix"]["length"];
+    for (let i = 0; i < length; i++) {
+        for (let y = 0; y < length; y++) {
+            console.log(i);
+            console.log(parsedData["matrix"][1][i]);
+        }
+    }
+
+
 }

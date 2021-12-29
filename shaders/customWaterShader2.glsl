4d7f2c7686476e7a666d26453b9908ca90525651
safalevent <safaleventoglu@gmail.com>
2021-12-30 01:43:30 +0300

    Updated UI and added ability to add and remove lights from scene. (We need to add an #if check on each shader to keep them working when there is no light on scene.)

--- shaders/customWaterShader2.glsl
+++ shaders/customWaterShader2.glsl
@@ -5,7 +5,9 @@ struct PointLight {
     vec3 position;
     float distance;
 };
+#if NUM_POINT_LIGHTS > 0
 uniform PointLight pointLights[NUM_POINT_LIGHTS];
+#endif
 
 varying vec3 vNormal;
 varying vec3 vPosition;
@@ -127,6 +129,8 @@ void main() {
     0.0,
     0.0,
     1.0);
+
+    #if NUM_POINT_LIGHTS > 0
     for (int l = 0; l < NUM_POINT_LIGHTS; l++) {
         vec3 distanceVec = vPosition - pointLights[l].position;
         distanceVec = distanceVec * 2.0;
@@ -139,6 +143,8 @@ void main() {
 
         addedLights.rgb += clamp(dot(-lightDirection, norm), 0.0, 1.0) * (pointLights[l].color * attuanation);
     }
+    #endif
+
     addedLights = max(vec4(0.3), addedLights);
     addedLights = min(vec4(1.5), addedLights);
     //----------- Lights -----------------

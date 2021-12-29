20bc69b0da15baf1f100278d74f792bc55cf6411
Kayra <kayraucklnc@gmail.com>
2021-12-29 20:05:07 +0300

    Water shader looks better

--- shaders/customWaterShader2.glsl
+++ shaders/customWaterShader2.glsl
@@ -20,6 +20,44 @@ uniform vec3 fogColor;
 uniform float fogDensity;
 uniform float u_time;
 
+vec3 hash(vec3 x)
+{
+    x = vec3(dot(x, vec3(127.1, 311.7, 74.7)),
+    dot(x, vec3(269.5, 183.3, 246.1)),
+    dot(x, vec3(113.5, 271.9, 124.6)));
+
+    return fract(sin(x)*43758.5453123);
+}
+
+vec3 voronoi2(in vec3 x)
+{
+    vec3 p = floor(x);
+    vec3 f = fract(x);
+
+    float id = 0.0;
+    vec2 res = vec2(100.0);
+    for (int k=-1; k<=1; k++)
+    for (int j=-1; j<=1; j++)
+    for (int i=-1; i<=1; i++)
+    {
+        vec3 b = vec3(float(i), float(j), float(k));
+        vec3 r = vec3(b) - f + hash(p + b);
+        float d = dot(r, r);
+
+        if (d < res.x)
+        {
+            id = dot(p+b, vec3(1.0, 57.0, 113.0));
+            res = vec2(d, res.x);
+        }
+        else if (d < res.y)
+        {
+            res.y = d;
+        }
+    }
+
+    return vec3(sqrt(res), abs(id));
+}
+
 float rand(vec2 co) {
     return fract(sin(dot(co, vec2(12.9898, 78.233))) * 43758.5453);
 }
@@ -83,22 +121,22 @@ vec3 voronoi(in vec2 x) {
     // first pass: regular voronoi
     vec2 mg, mr;
     float md = 8.0;
-    for (int j= -1; j <= 1; j++) {
-        for (int i= -1; i <= 1; i++) {
-            vec2 g = vec2(float(i), float(j));
-            vec2 o = random2(n + g);
-            o = 0.5 + 0.5*sin(6.2831*o);
-
-            vec2 r = g + o - f;
-            float d = dot(r, r);
-
-            if (d<md) {
-                md = d;
-                mr = r;
-                mg = g;
-            }
-        }
-    }
+    //    for (int j= -1; j <= 1; j++) {
+    //        for (int i= -1; i <= 1; i++) {
+    //            vec2 g = vec2(float(i), float(j));
+    //            vec2 o = random2(n + g);
+    //            o = 0.5 + 0.5*sin(6.2831*o);
+    //
+    //            vec2 r = g + o - f;
+    //            float d = dot(r, r);
+    //
+    //            if (d<md) {
+    //                md = d;
+    //                mr = r;
+    //                mg = g;
+    //            }
+    //        }
+    //    }
 
     // second pass: distance to borders
     md = 8.0;
@@ -106,7 +144,7 @@ vec3 voronoi(in vec2 x) {
         for (int i= -2; i <= 2; i++) {
             vec2 g = mg + vec2(float(i), float(j));
             vec2 o = random2(n + g);
-            o = 0.5 + 0.5*sin(6.2831*o);
+            o = 0.5 + 0.5*sin(u_time + 6.2831*o);
 
             vec2 r = g + o - f;
 
@@ -120,7 +158,8 @@ vec3 voronoi(in vec2 x) {
 //------------------------------
 
 void main() {
-    vec3 p =  worldPosition + vec3(u_time / 2.0, u_time, 0.0);
+    //    vec3 p =  worldPosition + vec3(u_time / 2.0, u_time, 0.0);
+    vec3 p =  worldPosition;
     //----------- Lights -----------------
     vec3 norm = vNormal;
     vec4 addedLights = vec4(0.0,
@@ -142,17 +181,23 @@ void main() {
     addedLights = max(vec4(0.3), addedLights);
     addedLights = min(vec4(1.5), addedLights);
     //----------- Lights -----------------
-    vec3 c = voronoi(p.xz / 5.0);
-    vec3 color = c.x*(0.788 + 0.532*sin(20.0*c.x))*vec3(0.160, 0.875, 1.000);
 
-    float colLen = smoothstep(0.0, 1.5, length(c.yz));
-    color = mix(vec3(0.247, 0.620, 1.000), vec3(0.387, 0.712, 1.000), colLen);
-    float scale = 2.0;
-    float a = snoise(p.xyz / 1.0) / scale;
-    float b = snoise(p.xyz / 1.0) / scale + 0.02;
+
+    //----------- Voronoi -----------------
+    vec3 color = vec3(0.);
+    vec3 c = voronoi(p.xz / 5.0);
+    float scale = 1.5;
+    float scale2 = 0.3;
+    float a = snoise(p.xyz / scale) * scale2;
+    float b = snoise(p.xyz / scale) * scale2;
+
+    vec3 newVor = voronoi2(vec3(p.xz * 0.4, u_time));
+    color = vec3(length(newVor.x));
+    float clen = smoothstep(0.0, 1.5, color.x);
+    color = mix(vec3(0.347, 0.702, 1.000), vec3(0.645, 0.942, 1.000), clen);
     color = mix(vec3(0.808, 0.911, 1.000), color, smoothstep(0.02 + a, 0.03 + b, c.x));
-    gl_FragColor = vec4(color, 1.);
-    gl_FragColor *= gl_FragColor * addedLights;
+    gl_FragColor = vec4(color, 1.0) * min(addedLights, 1.0);
+    //----------- Voronoi -----------------
 
 
     //--------- Fog -------------

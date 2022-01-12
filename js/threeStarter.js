0d8e8be6001d09fa4db9943629645e27c5829345
safalevent <safaleventoglu@gmail.com>
2022-01-13 01:16:01 +0300

    Deleted unnecessary debugging comments.

--- js/threeStarter.js
+++ js/threeStarter.js
@@ -1,8 +1,6 @@
 import * as Materials from "../../ecosystemsimulaton/js/world/Materials.js";
 import * as DataLoader from "../../ecosystemsimulaton/js/util/loadData.js";
 import {createInitScene, createInitControls, createTestSceneElements} from "./SceneOperations.js";
-import * as THREE from "../../ecosystemsimulaton/js/library/three.js-r135/build/three.module.js";
-import * as Objects from "../../ecosystemsimulaton/js/world/Objects.js";
 
 function threeStarter() {
     drawthechart();
@@ -15,29 +13,7 @@ function threeStarter() {
     createTestSceneElements();
     gui.setTerrain(terrainObject);
 
-    // var loopStats = new Stats();
-    // loopStats.showPanel(0);
-    // document.body.appendChild(loopStats.dom);
-
-    // let cameraOffset = new THREE.Vector3(0, 4, 4);
-    // let followObject = world.objects.filter((x) => {
-    //     return x instanceof Objects.Pig;
-    // })[0];
-    // followObject.hasDied = true;
-    // let oldpos = new THREE.Vector3().copy(followObject.getPos());
-    //
-    // let newPos = new THREE.Vector3().copy(followObject.mesh.position).add(cameraOffset);
-    // camera.position.set(newPos.x, newPos.y, newPos.z);
-
     function loop() {
-        // loopStats.begin();
-        // if (oldpos.distanceToSquared(followObject.getPos()) > 0.2) {
-        //     let moveAmn = new THREE.Vector3().subVectors(followObject.getPos(), oldpos);
-        //     camera.position.add(moveAmn);
-        //     oldpos = new THREE.Vector3().copy(followObject.getPos());
-        // }
-        // orbitControls.target = followObject.mesh.position;
-
         raycaster.setFromCamera(mouse, camera);
 
         controls.update();
